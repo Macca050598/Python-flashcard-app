b2ec456b12a8cc79917961df422cb8a21f901be2
Macca Williams <mackenzie050598@gmail.com>
2024-12-13 07:12:09 +0000

    Flashcard app created

--- main.py
+++ main.py
@@ -6,26 +6,45 @@ import csv
 import random
 
 header = ["id", "question", "answer"]
-    
+row = []
 def flashcard_app():
-    print("Welcome to the flashcard app")
-    print("Remember, each time you close the app it'll delete your flashcards")
-    print("1. Add a question and answer")
-    print("2. Test yourself")
-    print("3. Exit the game...")
-    choice = input("Choose an option 1-3")
-    
-    if choice == "1":
-       flashcard = questions_Answers()
-       f = open("flashcardData.csv", mode="w", newline="")
-       writer = csv.writer(f)
-       writer.writerow(header)
-       writer.writerow(flashcard)
-    elif choice == "2":
-        question_picker()
-    elif choice == "3":
-        return
-    
+    while True:
+        print("Welcome to the flashcard app")
+        print("Remember, each time you close the app it'll delete your flashcards")
+        print("1. Add a question and answer")
+        print("2. Test yourself")
+        print("3. Delete all flashcards")
+        print("4. Exit the game...")
+        choice = input("Choose an option 1-4: ")
+        
+        if choice == "1":
+         flashcard = questions_Answers()
+         f = open("flashcardData.csv", mode="a", newline="")
+         writer = csv.writer(f)
+         writer.writerow(header)
+         writer.writerow(flashcard)
+         return
+        elif choice == "2":
+            try: 
+                f = open("flashcardData.csv")
+                reader = csv.reader(f)
+                next(reader)
+                questions = list(reader)
+                if not questions:
+                    print("No questions are available. Please add some first.")
+                    return
+                question = random.choice(questions)
+                print(f"Question: {question[1]}")
+                input("Your Answer: ")
+                print(f"The correct answer is: {question[2]}")    
+            except FileNotFoundError: 
+                    print("No flashcards found. Please add some first.")
+        elif choice == "3":
+            f = open("flashcardData.csv", mode="w")
+            f.truncate()
+            f.close()
+        elif choice == "4":
+            return
 
 def questions_Answers():
     question = input("Please enter your question: ")
@@ -34,11 +53,6 @@ def questions_Answers():
     number = random.randint(1, 1000)
     
     return [number, question, answer]
-
-
-def question_picker():
-    question = ["Test", "Twlooqw"]
-    print(random.choice(question))
 
 def main():
     flashcard_app()
